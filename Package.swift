6bc1a1eac370e61bbf56dc0c8d3156b91e92ce10
Chris Araman <chris@hiddenplace.dev>
2021-12-20 21:39:49 -0800

    ♻️ Require Swift 5.5

--- Package.swift
+++ Package.swift
@@ -1,39 +1,35 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 
 import PackageDescription
 
 let package = Package(
-  name: "CombineCloudKit",
-  // Combine requires macOS 10.15, iOS 13, or tvOS 13.
+  name: "AsyncCloudKit",
+  // Concurrency for macOS 12, MacCatalyst 15, iOS 15, tvOS 15, or watchOS 8 requires Swift 5.5 or Xcode 13.
+  // Concurrency for macOS 10.15, Mac Catalyst 13, iOS 13, tvOS 13, or watchOS 6 requires Swift 5.5.2 or Xcode 13.2.1.
+  // XCTest requires watchOS 7.4.
   platforms: [
+    .macCatalyst(.v13),
     .macOS(.v10_15),
     .iOS(.v13),
     .tvOS(.v13),
+    .watchOS("7.4"),
   ],
   products: [
     .library(
-      name: "CombineCloudKit",
-      targets: ["CombineCloudKit"]
-    )
+      name: "AsyncCloudKit",
+      targets: ["AsyncCloudKit"]
+    ),
+  ],
+  dependencies: [
+    // Used by script/lint. Assumes current Swift tools.
+    .package(url: "https://github.com/apple/swift-format.git", .branch("swift-5.5-branch"))
   ],
   targets: [
-    .target(name: "CombineCloudKit"),
+    .target(name: "AsyncCloudKit"),
     .testTarget(
-      name: "CombineCloudKitTests",
-      dependencies: ["CombineCloudKit"]
+      name: "AsyncCloudKitTests",
+      dependencies: ["AsyncCloudKit"]
     ),
   ],
   swiftLanguageVersions: [.v5]
 )
-
-#if swift(>=5.4)
-  // XCTest for watchOS requires watchOS 7.4 and Swift 5.4.
-  package.platforms! += [
-    .watchOS("7.4")
-  ]
-#else
-  // Combine requires watchOS 6.
-  package.platforms! += [
-    .watchOS(.v6)
-  ]
-#endif
