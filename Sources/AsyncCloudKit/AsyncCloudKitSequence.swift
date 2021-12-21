5614275ab9f5db1e1280f927ff21c8e4c61301f5
Chris Araman <chris@hiddenplace.dev>
2021-12-20 23:46:01 -0800

    📖 Build docs

--- Sources/AsyncCloudKit/AsyncCloudKitSequence.swift
+++ Sources/AsyncCloudKit/AsyncCloudKitSequence.swift
@@ -6,11 +6,19 @@
 //  Copyright © 2021 Chris Araman. All rights reserved.
 //
 
+/// An [`AsyncSequence`](https://developer.apple.com/documentation/swift/asyncsequence)
+/// of results from an AsyncCloudKit operation. If this sequence is iterated twice,
+/// no operation is queued, and no results are returned.
 public struct AsyncCloudKitSequence<Element>: AsyncSequence {
   public typealias AsyncIterator = AsyncCloudKitIterator<Element>
   public typealias Element = Element
 
+  /// An [`AsyncIteratorProtocol`](https://developer.apple.com/documentation/swift/asynciteratorprotocol)
+  /// of results from an AsyncCloudKit operation.
+  /// - SeeAlso: [`AsyncSequence`](https://developer.apple.com/documentation/swift/asyncsequence)
   public struct AsyncCloudKitIterator<Element>: AsyncIteratorProtocol {
+
+    /// The type of result from an AsyncCloudKit operation to iterate over.
     public typealias Element = Element
 
     private let upstreamNext: () async throws -> Element?
@@ -23,6 +31,10 @@ public struct AsyncCloudKitSequence<Element>: AsyncSequence {
       }
     }
 
+    /// Asynchronously advances to the next result and returns it, or ends the
+    /// sequence if there is no next result.
+    /// - Returns: The next result, if it exists, or `nil` to signal the end of
+    ///   the sequence.
     public mutating func next() async throws -> Element? {
       try await upstreamNext()
     }
@@ -36,10 +48,15 @@ public struct AsyncCloudKitSequence<Element>: AsyncSequence {
     }
   }
 
+  /// Creates an ``AsyncCloudKitIterator`` of results from an AsyncCloudKit operation.
+  /// - Returns: The ``AsyncCloudKitIterator``.
+  /// - SeeAlso: [`AsyncSequence`](https://developer.apple.com/documentation/swift/asyncsequence)
   public func makeAsyncIterator() -> AsyncCloudKitIterator<Element> {
     makeAsyncCloudKitIterator()
   }
 
+  /// Collects all of the results from the sequence and returns them as a single array.
+  /// - Returns: An array of the results.
   public func collect() async throws -> [Element] {
     try await reduce(into: [Element]()) { $0.append($1) }
   }
